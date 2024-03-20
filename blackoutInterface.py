c5fd5ab3cab5e4ff59d53fa909087d9d5ad6bb62
Kate Schwitz <katiejoschwitz@gmail.com>
2024-03-20 15:09:22 -0400

    organized asset images, changed collision logic to only collect/avoid if jumping/sliding

--- blackoutInterface.py
+++ blackoutInterface.py
@@ -9,6 +9,7 @@ WIDTH, HEIGHT = 800, 600
 GROUND_HEIGHT = 50
 FPS = 60
 OBS_SPEED = .1
+TRAIN_SPEED = .7
 
 SCORE = 0
 LIVES = 3
@@ -18,13 +19,13 @@ CENTER = WIDTH /2
 RIGHT = WIDTH * .75
 
 #load assests
-player_img = pygame.image.load('runner.png')
+player_img = pygame.image.load('assets/runner.png')
 player_img = pygame.transform.scale(player_img, (50, 100))
-coin_img = pygame.image.load('coin.png')
+coin_img = pygame.image.load('assets/coin.png')
 coin_img = pygame.transform.scale(coin_img, (50, 50))
-sign_img = pygame.image.load('sign.png')
+sign_img = pygame.image.load('assets/sign.png')
 sign_img = pygame.transform.scale(sign_img, (50, 100))
-train_img = pygame.image.load('train.jpg')
+train_img = pygame.image.load('assets/train.jpg')
 train_img = pygame.transform.scale(train_img, (70, 70))
 
 #define player variables
@@ -48,6 +49,9 @@ sign2_y = -HEIGHT * 1.5
 
 coin1_x = CENTER
 coin1_y = -HEIGHT * .7
+
+train1_x = CENTER
+train1_y = -HEIGHT * 3
 # Set up the drawing window
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption("BlackOUT")
@@ -76,7 +80,7 @@ while running:
         if player_lane == 1: player_x = RIGHT      
 
         if player_jump:
-            player_img = pygame.image.load('jump.png')
+            player_img = pygame.image.load('assets/jump.png')
             player_img = pygame.transform.scale(player_img, (50, 100))
             screen.blit(player_img, (player_x, player_y))
             player_y -= jump_height
@@ -87,7 +91,7 @@ while running:
                 player_y += 55
         else:
             if player_slide: 
-                player_img = pygame.image.load('slide.png')
+                player_img = pygame.image.load('assets/slide.png')
                 player_img = pygame.transform.scale(player_img, (50, 100))
                 screen.blit(player_img, (player_x, player_y))
                 player_y -= slide_height
@@ -97,22 +101,35 @@ while running:
                     slide_height = -10
                     player_y -= 55
             else:
-                player_img = pygame.image.load('runner.png')
+                player_img = pygame.image.load('assets/runner.png')
                 player_img = pygame.transform.scale(player_img, (50, 100))
 
     #obstacle movements
     sign1_y += OBS_SPEED
     sign2_y += OBS_SPEED
     coin1_y += OBS_SPEED
+    train1_y += TRAIN_SPEED
     #collision detection 
     if player_invulnerable_frames >0: player_invulnerable_frames -=1      
     player_rect = pygame.Rect(player_x, player_y, player_img.get_width(), player_img.get_height())
     sign1_rect = pygame.Rect(sign1_x, sign1_y, sign_img.get_width(), sign_img.get_height())
+    sign2_rect = pygame.Rect(sign2_x, sign2_y, sign_img.get_width(), sign_img.get_height())
     coin1_rect = pygame.Rect(coin1_x, coin1_y, coin_img.get_width(), coin_img.get_height())
-    if player_rect.colliderect(sign1_rect) and player_invulnerable_frames ==0:
+    train1_rect = pygame.Rect(train1_x, train1_y, train_img.get_width(), train_img.get_height())
+    #SIGNS
+    if player_rect.colliderect(sign1_rect) and player_invulnerable_frames ==0 and player_slide == False:
         LIVES -= 1
         player_invulnerable_frames = 2100
-    if player_rect.colliderect(coin1_rect) and player_invulnerable_frames ==0:
+    if player_rect.colliderect(sign2_rect) and player_invulnerable_frames ==0 and player_slide == False:
+        LIVES -= 1
+        player_invulnerable_frames = 2100
+    #TRAINS
+    if player_rect.colliderect(train1_rect) and player_invulnerable_frames ==0:
+        LIVES -= 1
+        player_invulnerable_frames = 2100
+
+    #COINSE
+    if player_rect.colliderect(coin1_rect) and player_invulnerable_frames ==0 and player_jump == True:
         SCORE +=1
         player_invulnerable_frames = 1800
     # Fill the background with white
@@ -126,6 +143,7 @@ while running:
     screen.blit(sign_img, (sign1_x, sign1_y))
     screen.blit(sign_img, (sign2_x, sign2_y))
     screen.blit(coin_img, (coin1_x, coin1_y))
+    screen.blit(train_img, (train1_x, train1_y))
     #display score/lives
     font = pygame.font.Font(None, 36)
     score_text = font.render(f"Score: {SCORE}", True, (0,0,0))
@@ -134,6 +152,6 @@ while running:
     screen.blit(lives_text, (20, 40))
     # Flip the display
     pygame.display.flip()
-
+    if LIVES == 0: running = False
 # Done! Time to quit.
 pygame.quit()
