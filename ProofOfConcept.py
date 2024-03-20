6e86e738e410ee7c16f33753c90d8825f76d179a
Pierce Casey <ppj679o@gmail.com>
2024-03-20 19:10:20 -0400

    Audio rotates clockwise around user over the song's duration (~1:30)

--- ProofOfConcept.py
+++ ProofOfConcept.py
@@ -1,32 +1,71 @@
+import glob, pygame, os
 import numpy as np
 import soundfile as sf
-import playsound 
-import sys, glob
-
+from pydub import AudioSegment
+from scipy.io.wavfile import write
 
 #Loads list of KEMAR angles to be used by player
-kemar = '.\KEMAR\diffuse\elev0\*.wav'
+kemar = './KEMAR/full/elev0/*.wav'
 KEMAR = glob.glob(kemar)
-for s in (range(len(KEMAR))):
-    print(KEMAR[s])
 
-#44100 sample of hemisphere around KEMAR at elevation 0, location chosen randomly
-[HRTF, sample] = sf.read(KEMAR[15])
+#After splitting file, loop through each KEMAR angle and create a binaural mix to give the illusion of moving sound clockwise
+for i in (range(0, int((len(KEMAR) / 2) - 1), 1)):
+    t1 = i * 1300
+    t2 = t1 + 1300
+    
+    #Loads audio file and snippet that is to be convolved 
+    snippet = AudioSegment.from_wav("./Sounds/BackOnMyBS.wav")
+    snippet = snippet[t1:t2]
+    
+    #Must export snippet as .wav file to read NumPY array 
+    snippet.export("rotate.wav", format="wav")
+    [bsHRTF, bsSample] = sf.read("rotate.wav")
+    bsHRTF = np.mean(bsHRTF, axis=1)
+    
+    #HRTF in clockwise circle gathered and convolved with current snippet
+    [LHRTF, sample] = sf.read(KEMAR[i])
+    [RHRTF, sample] = sf.read(KEMAR[i + 72])
+    L = np.convolve(bsHRTF, LHRTF)
+    R = np.convolve(bsHRTF, RHRTF)
+    
+    #Binaural mix NumPY array is created, this is the desired final audio data 
+    Bin_Mix = np.vstack([L,R]).transpose()
+    
+    #NOTE: The original idea was to be streaming this audio and have the binaural mix play directly here, but the lag time in the audio was so severe that it was not possiible. 
+    #I am not sure if a library that can play NumPy arrays such as what is required even exists in Python:
+    # - Sounddevice is not allowed to queue audio, so there are major gaps in the produced sound
+    # - Playsound does not allow for NumPy arrays 
+    # - PyAudio does not allow for NumPy arrays 
+    #Instead, my approach was to save each chunk and play them directly as .wav files, deleting them after use: 
+    name = "rotate" + str(i) + ".wav"
+    write(name, 44611, Bin_Mix.astype(np.float32))
+    
 
-#TODO: translate stereo sounds to mono; boing track is only inherent mono track available so far
+#Establish pygame mixer queue to load audio quicker than other libraries in Python
+pygame.init()
+pygame.mixer.init()
+clock = pygame.time.Clock()
 
-#Gathers attributes from 
-[BoingHRTF, boingSample] = sf.read(".\Sounds\Boing.wav")
-print(boingSample)
-print(BoingHRTF.shape)
+#Load each chunk into pygame mixer and play through queue
+j = 1
+for i in range(0, 70, 2):
+    name1 = "rotate" + str(i) + ".wav"
+    name2 = "rotate" + str(j) + ".wav"
+    j += 2
+    
+    pygame.mixer.music.load(name1)
+    pygame.mixer.music.queue(name2)
+    pygame.mixer.music.play()
+    
+    #Loop makes sure the mixer does not skip any parts of queue, could be leading to the delay of audio chunks
+    while pygame.mixer.music.get_busy():
+        clock.tick(30)
 
-#Convolves the audio file with each column of KEMAR HRTF representing left and right inputs
-L = np.convolve(BoingHRTF, HRTF[:,0])
-R = np.convolve(BoingHRTF, HRTF[:,1])
-Bin_Mix = np.vstack([L,R]).transpose()
+#Cleanup 
+pygame.quit()
+os.remove("rotate.wav")
 
-#Saves binaural mix as .wav to be played by playsound
-sf.write('SpatializedBoing.wav', Bin_Mix, boingSample)
-print(Bin_Mix.shape)
-
-playsound.playsound('SpatializedBoing.wav')
+#Delete chunk files from local OS
+for i in range(0, 71, 1):
+    name = "rotate" + str(i) + ".wav"
+    os.remove(name)
---
+++ README.txt
@@ -0,0 +1,18 @@
+Platform: VS Code 
+Python Environment Version: 3.11.8
+Python Dependencies: glob, pygame, os, numpy, soundfile, pydub, scipy (Installed via "pip install [dependency]")
+
+Steps: 
+1. Unzip File
+2. Create 3.11.8 Python Environment
+3. Download dependencies via pip in console 
+4. Run via VS Code 
+
+*Path Dependencies are handled in program, no further action required*
+
+DISCLAIMER: I am aware that this is not the most optimal way to recreate 3D audio, as I would have preferred to do convolutions of chunks of streamed data and played directly to make the process as quick as possible.
+However, I did not find a way to accomplish this in Python after attempting to use practically every audio library in the language such as sounddevice, playsound, pyaudio, pydub, winsound, and many others. 
+This is reflected in the small auditory silence between each chunk of rendered data; the illusion of spatial sound is still present, but the transition between them is choppy at best. 
+This could be due to many things, such as the time it takes to sample the audio file, the limitations of play modules in Python, lack of specs on the system, and many more. 
+My ultimate goal is to stream the audio, do my operations, and queue the processed chunk while continuing, but I could not find a homogenous way to achieve this in Python. 
+We will have to come up with other ways to achieve this task, as I fear that using this approach during our app development will have major problems with updating audio. 
