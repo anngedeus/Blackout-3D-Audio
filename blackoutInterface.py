c8a019734a0bb2a86c817540006c6264626a5bbb
nehemieyac <74078487+nehemieyac@users.noreply.github.com>
2024-04-23 00:18:07 -0400

    Update blackoutInterface.py

--- blackoutInterface.py
+++ blackoutInterface.py
@@ -5,9 +5,53 @@ import pygame, glob
 import numpy as np
 import soundfile as sf
 from pydub import AudioSegment
-import librosa
+import time
+
 
 pygame.init()
+
+def audioProcessing(): 
+    kemar = 'ProofOfConcept/KEMAR/full/elev0/*.wav'
+    KEMAR = glob.glob(kemar)
+
+    audio = AudioSegment.from_wav("Audio/TrainFinal.wav")
+
+    sampleRate = 200
+
+    j = 0
+    for i in range(0, len(audio), sampleRate):
+        #Creates sample based on audio length and sample rate variable
+        HRTF = audio[i:i + sampleRate]
+        
+        #Takes array from audio chunk streamed 
+        HRTF = np.array(HRTF.get_array_of_samples(), dtype=np.float32)
+        HRTF /= np.max(np.abs(HRTF))
+        
+        #HRTF  convolved with current snippet
+        [LHRTF, sample] = sf.read(KEMAR[j])
+        [RHRTF, sample] = sf.read(KEMAR[j + 72])
+        
+        #Logic check for what HRTF to use 
+        j += 1
+        if j == 36:
+            j = 0
+        L = np.convolve(HRTF, LHRTF)
+        R = np.convolve(HRTF, RHRTF)
+        
+        #Binaural mix NumPY array is created, this is the desired final audio data 
+        Bin_Mix = np.vstack([L,R]).transpose()
+        
+        #Scales Binary Mix and makes it a contiguous array 
+        Bin_Mix_scaled = (Bin_Mix * 32767).astype(np.int16)
+        Bin_Mix_scaled = np.ascontiguousarray(Bin_Mix_scaled)
+        
+        #Creates and plays sound based on array
+        sound = pygame.sndarray.make_sound(Bin_Mix_scaled)
+        sound = pygame.mixer.Sound(sound)
+        sound.play()
+        
+        #Toy around with time for time.sleep to work properly on different system 
+        time.sleep(0.195)
 
 #game constants
 WIDTH, HEIGHT = 800, 600
@@ -75,7 +119,7 @@ pygame.display.set_caption("BlackOUT")
 running = True
 
 while running:
-    
+        
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -146,6 +190,17 @@ while running:
     coin1_y += OBS_SPEED
     train1_y += TRAIN_SPEED
     
+    # Calculate listener position
+    player_position = (player_x, player_y)
+    
+    # Calculate distance and direction from listener to object
+    object_position = (train1_x, train1_y)
+    distance = np.linalg.norm(np.array(player_position) - np.array(object_position))
+    direction = (np.array(object_position) - np.array(player_position))  #/ np.linalg.norm(distance)
+    
+    #Calculate HRTF and get the audio
+    audioProcessing()
+    
     #collision detection 
     if player_invulnerable_frames >0: player_invulnerable_frames -=1      
     player_rect = pygame.Rect(player_x, player_y, player_img.get_width(), player_img.get_height())
@@ -195,3 +250,4 @@ while running:
     if LIVES == 0: running = False
 # Done! Time to quit.
 pygame.quit()
+
